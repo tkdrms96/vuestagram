a5d11122d024734638b57746e17e2a47dc78106a
ishift <ishift2023>
2023-05-08 15:32:02 +0900

    create de

---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Post :data = "data[i]" v-for="(a,i) in data" :key="i"/>
+</div>
+</template>
+
+<script>
+
+import Post from './Post.vue'
+
+export default {
+    components : {
+        Post, 
+    },
+    props : {
+        data : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
